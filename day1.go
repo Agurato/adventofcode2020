42b0228c6e8b939dae0bd4f2e32e9880b8c64335
Agurato <Agurato@users.noreply.github.com>
2020-12-01 18:20:39 +0100

    small optimisation for day 1

--- day1.go
+++ day1.go
@@ -16,9 +16,10 @@ func day1Part1() int {
 		panic(err)
 	}
 
-	for index1 := 0; index1 < len(values)-1; index1++ {
+	valuesLen := len(values)
+	for index1 := 0; index1 < valuesLen-1; index1++ {
 		value1 := values[index1]
-		for index2 := index1 + 1; index2 < len(values); index2++ {
+		for index2 := index1 + 1; index2 < valuesLen; index2++ {
 			value2 := values[index2]
 			if value1+value2 == 2020 {
 				return value1 * value2
@@ -34,10 +35,12 @@ func day1Part2() int {
 		panic(err)
 	}
 
-	for index1, value1 := range values {
-		for index2 := index1 + 1; index2 < len(values)-1; index2++ {
+	valuesLen := len(values)
+	for index1 := 0; index1 < valuesLen-2; index1++ {
+		value1 := values[index1]
+		for index2 := index1 + 1; index2 < valuesLen-1; index2++ {
 			value2 := values[index2]
-			for index3 := index2 + 1; index3 < len(values); index3++ {
+			for index3 := index2 + 1; index3 < valuesLen; index3++ {
 				value3 := values[index3]
 				if value1+value2+value3 == 2020 {
 					return value1 * value2 * value3
