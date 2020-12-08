a47e234a23f502aef22fa7422eeda1cd1be66ac6
Agurato <Agurato@users.noreply.github.com>
2020-12-08 12:44:49 +0100

    day 8 part 2

--- day8.go
+++ day8.go
@@ -45,5 +45,56 @@ func day8Part1(values []string) int {
 }
 
 func day8Part2(values []string) int {
-	return 0
+	oldInst := ""
+	accumulator := 0
+	lenValues := len(values)
+	for tryI, try := range values {
+		switch try[:3] {
+		case "nop":
+			oldInst = "nop"
+			values[tryI] = "jmp" + try[3:]
+		case "jmp":
+			oldInst = "jmp"
+			values[tryI] = "nop" + try[3:]
+		default:
+			continue
+		}
+
+		executed := make(map[int]bool)
+		accumulator = 0
+		lineI := 0
+		finished := false
+		for {
+			// If we already went through this line, quit with failure
+			if _, ok := executed[lineI]; ok {
+				break
+			}
+			// If we reached the end of the program, quit with success
+			if lineI >= lenValues {
+				finished = true
+				break
+			}
+			executed[lineI] = true
+			line := values[lineI]
+			instruction := strings.Split(line, " ")
+			switch instruction[0] {
+			case "acc":
+				value, _ := strconv.Atoi(instruction[1])
+				accumulator += value
+				fallthrough
+			case "nop":
+				lineI++
+			case "jmp":
+				value, _ := strconv.Atoi(instruction[1])
+				lineI += value
+			}
+		}
+		if finished {
+			break
+		}
+
+		values[tryI] = oldInst + try[3:]
+	}
+
+	return accumulator
 }
